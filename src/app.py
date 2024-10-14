4ac9e2081d3be5da1d5ed4e067562a8074cfccca
StaGlo <stanley.glowczewski@gmail.com>
2024-10-14 19:00:27 +0200

    Initial commit
    Co-authored-by: Rafał Szubert <rafixxx4k@users.noreply.github.com>
    Co-authored-by: Kacper Majorkowski <kmkacpermajor@users.noreply.github.com>
    Co-authored-by: Marci Łukomski <marcinlukomski@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -1,15 +1,67 @@
 # Description: This file is the main file for the server. It will handle all the requests and responses from the client.
-from flask import Flask, render_template, request
-#from waitress import serve
+# from waitress import serve
+import crochet
+
+crochet.setup()
+
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from scrapy import signals
+from scrapy.crawler import CrawlerRunner
+from scrapy.signalmanager import dispatcher
+import time
+import os
+
+# Importing our Scraping Function from the file
+
+from business.web2ai.spiders.pcss import PcssSpider
 
 app = Flask(__name__)
 
-@app.route('/')
+output_data = []
+crawl_runner = CrawlerRunner()
 
+
+# By Deafult Flask will come into this when we run the file
+@app.route("/")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
+
+
+# After clicking the Submit Button FLASK will come into this
+@app.route("/", methods=["POST"])
+def submit():
+    if request.method == "POST":
+        s = request.form["url"]
+        global baseURL
+        baseURL = s
+
+        return redirect(url_for("scrape"))  # Passing to the Scrape function
+
+
+@app.route("/scrape")
+def scrape():
+
+    scrape_with_crochet(baseURL=baseURL)  # Passing that URL to our Scraping Function
+
+    time.sleep(20)  # Pause the function while the scrapy spider is running
+
+    return "GIT"
+
+
+@crochet.run_in_reactor
+def scrape_with_crochet(baseURL):
+    # This will connect to the dispatcher that will kind of loop the code between these two functions.
+    dispatcher.connect(_crawler_result, signal=signals.item_scraped)
+
+    # This will connect to the ReviewspiderSpider function in our scrapy file and after each yield will pass to the crawler_result function.
+    eventual = crawl_runner.crawl(PcssSpider, url=baseURL)
+    return eventual
+
+
+# This will append the data to the output data list.
+def _crawler_result(item, response, spider):
+    output_data.append(dict(item))
+
 
 if __name__ == "__main__":
-    app.run(debug=True) #default port is 5000, look at the terminal to see the port number
-    #app.run(host="0.0.0.0", port="8000", debug=True)
-    #serve(app, host="0.0.0.0", port="8000")
+    app.run(debug=True)
---
+++ src/business/web2ai/spiders/pcss.py
@@ -0,0 +1,46 @@
+import scrapy
+from bs4 import BeautifulSoup
+import datetime
+
+
+class PcssSpider(scrapy.Spider):
+    name = "pcss"
+
+    def __init__(
+        self, url="", **kwargs
+    ):  # The category variable will have the input URL.
+        self.url = url
+        super().__init__(**kwargs)
+
+    custom_settings = {
+        "CLOSESPIDER_TIMEOUT": 15,
+    }  # This will tell scrapy to store the scraped data to outputfile.json and for how long the spider should run.
+
+    def stripTags(self, response):
+        # Use BeautifulSoup to clean the HTML
+        soup = BeautifulSoup(response.body, "html.parser")
+
+        # Remove script and style tags
+        for tag in soup(["script", "style", "head", "link"]):
+            tag.decompose()  # Remove the tag and its content
+            # Remove elements with id containing "nav"
+        for tag in soup.find_all(id=lambda x: x and "nav" in x):
+            tag.decompose()
+        for tag in soup.find_all(className=lambda x: x and "nav" in x):
+            tag.decompose()
+
+        # Save the cleaned HTML
+        return soup.prettify(encoding="utf-8")
+
+    def start_requests(self):
+        request = scrapy.Request(self.url, callback=self.parse)
+
+        yield request
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        filename = f"pcss-{page}-{timestamp}.html"
+
+        with open(filename, "wb") as f:
+            f.write(self.stripTags(response))
